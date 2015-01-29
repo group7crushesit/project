1635e0794dc3481c56d4b490af2754a7c768b311
Bryan Hernandez <Bryanhl.mail@gmail.com>
2015-01-28 17:18:15 -0800

    Revert "fixes and error correction"

--- project_2/config/routes.rb
+++ project_2/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root to: "sites#index" 
 
+  get "/login", to: "sessions#new"
 
-  root to: "sites#index"
-
-  resources :users, except: [:index, :new]
+  resources :users
 
   resources :boards
 
@@ -11,10 +11,8 @@ Rails.application.routes.draw do
   
   get "/sign_up", to: "users#new", as: "sign_up"
 
-  # post "/sessions", to: "sessions#create"
-  get "/login", to: "sessions#new", as: "login"
+  get "/logout", to: "sessions#destroy"
 
-  resources :sessions, only: [:create, :destroy]
   post "/sessions", to: "sessions#create"
 end
 
