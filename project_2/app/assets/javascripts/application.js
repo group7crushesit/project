1fdbfa60dbb80d8a6e04dff32129ca0ffce11b57
Ajesh Shah <ajeshshah@gmail.com>
2015-01-29 14:30:06 -0800

    pagination with attempted scroll load

--- project_2/app/assets/javascripts/application.js
+++ project_2/app/assets/javascripts/application.js
@@ -17,3 +17,15 @@
 //= sdfs
 //= require_tree .
 //= require bootstrap/dist/js/bootstrap
+$(document).ready(function() {
+  if ($('.pagination').length) {
+    $(window).scroll(function() {
+      var url = $('.pagination .next_page').attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+        $('.pagination').text("Please Wait...");
+        return $.getScript(url);
+      }
+    });
+    return $(window).scroll();
+  }
+});
